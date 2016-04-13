40b60bd51cfa16bfd8d1b81305e91f8f5be33cdb
tianzhongbo <tianzhongbo@douban.com>
2016-04-13 13:40:44 +0800

    add mnist example

---
+++ examples/mnist/mnist.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+from input_data import read_data_sets
+
+x = tf.placeholder(tf.float32, [None, 784])
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+y = tf.nn.softmax(tf.matmul(x, W) + b)
+y_ = tf.placeholder(tf.float32, [None, 10])
+cross_entropy = -tf.reduce_sum(y_*tf.log(y))
+train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
+init = tf.initialize_all_variables()
+
+mnist = read_data_sets("MNIST_data/", one_hot=True)
+with tf.Session() as sess:
+    sess.run(init)
+    for i in range(1000):
+        batch_xs, batch_ys = mnist.train.next_batch(100)
+        sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+
+    correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
